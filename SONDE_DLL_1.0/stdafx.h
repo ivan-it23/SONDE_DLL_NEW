3c3d9c01764baf55de45f64d93888a950d250320
ivan-it23 <ivanboev.2005@yandex.ru>
2026-05-17 11:17:31 +0300

    Первоначальная фиксация приложения после переноса на Visual Studio 2026

--- SONDE_DLL_1.0/stdafx.h
+++ SONDE_DLL_1.0/stdafx.h
@@ -7,10 +7,10 @@
 
 #include "targetver.h"
 
+#include <complex>
+
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
 #include <windows.h>
-
-
 
 // установите здесь ссылки на дополнительные заголовки, требующиеся для программы
